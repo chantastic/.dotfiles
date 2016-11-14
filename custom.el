8d66d9aa252e8f52fa94a856fa6d49b5b4c2beec
Michael Chan <mijoch@gmail.com>
2016-11-14 07:53:01 -0800

    emacs: make Hasklig default font and inhibit startup screen

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(fringe-mode 0 nil (fringe))
+ '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
     (magit editorconfig helm-ag helm-projectile projectile helm evil-surround evil)))
@@ -15,4 +16,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Hasklig")))))
