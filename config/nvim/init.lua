f36e404d602797ee8fa49d8bfbf3ae1f07cc2014
Michael Chan <mijoch@gmail.com>
2023-12-29 23:21:29 -0800

    Add nvim-surround plugin for to neovim (via lazy)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,4 +14,14 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
 })
