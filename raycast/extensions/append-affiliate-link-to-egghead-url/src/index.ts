38278a255a5b7ba881e1eed309f555fdbf029dcc
Michael Chan <mijoch@gmail.com>
2024-01-16 14:05:22 -0800

    tailor affiliate link appending experience i want (and errors)

--- raycast/extensions/append-affiliate-link-to-egghead-url/src/index.ts
+++ raycast/extensions/append-affiliate-link-to-egghead-url/src/index.ts
@@ -1,7 +1,36 @@
-import { showHUD, Clipboard } from "@raycast/api";
+import { Clipboard, getSelectedText, showToast, Toast } from "@raycast/api";
+
+// NTH: configurable affiliate link
+function appendAffiliateLink(incomingURL: string) {
+  const modifiedURL = new URL(incomingURL);
+  modifiedURL.searchParams.set("af", "1x80ad");
+
+  return modifiedURL.toString();
+}
 
 export default async function main() {
-  const now = new Date();
-  await Clipboard.copy(now.toLocaleDateString());
-  await showHUD("Copied date to clipboard");
+  try {
+    const selectedText = await getSelectedText();
+
+    if (!selectedText) {
+      throw new Error("No text selected.");
+    }
+
+    if (!selectedText.includes("egghead.io")) {
+      throw new Error("URL not supported.");
+    }
+
+    const appendedText = appendAffiliateLink(selectedText);
+
+    await Clipboard.copy(appendedText);
+    await Clipboard.paste(appendedText);
+  } catch (error) {
+    if (error instanceof Error) {
+      await showToast({
+        style: Toast.Style.Failure,
+        title: error.message ? error.message : "Uknown error.",
+        message: "Please try again.",
+      });
+    }
+  }
 }
